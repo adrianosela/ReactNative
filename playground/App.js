7de59fd979f120f759d8ed5a709ef32bd5527faf
Adriano <adriano.selaviles@gmail.com>
2019-04-02 14:56:30 -0700

    add ws

--- playground/App.js
+++ playground/App.js
@@ -2,12 +2,12 @@
  * Qube State Livestream Client for wss://live.qubichub.xyz/watch
  */
 import React, { Component } from 'react'
-import { AppRegistry, View } from 'react-native'
+import { View, Text } from 'react-native'
 import WS from 'react-native-websocket'
- 
+
 export default class LiveStream extends Component {
 
-	constructor(props) {
+  constructor(props) {
 		super(props);
 		this.state = {
 			viewers: null,
@@ -17,23 +17,23 @@ export default class LiveStream extends Component {
 
   render () {
     return (
-      <View style={{flex: 1}}>
-        <WS
-          ref={ref => {this.ws = ref}}
-          url="wss://live.qubichub.xyz/watch"
-          onOpen={() => {
-            console.log('live stream websockets connection open')
-          }}
-          onMessage={(e) => {
-          	// Steven: here instead of logging, parse the qube state and send
-          	// it to bluetooth
-          	const data = JSON.parse(e.data)
-          	this.setState({viewers: data.viewers, qube: data.state});
-          	console.log(this.state);
-          }}
-          onError={(e) => console.log(JSON.parse(e.data))}
-          onClose={console.log}
-        />
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <WS
+      ref={ref => {this.ws = ref}}
+      url="wss://live.qubichub.xyz/watch"
+      onOpen={() => {
+        console.log('live stream websockets connection open')
+      }}
+      onMessage={(e) => {
+        const data = JSON.parse(e.data)
+        this.setState({viewers: data.viewers, qube: data.state});
+        // TODO: instead of logging, parse the qube state and plot with bluetooth wrapper
+        console.log(this.state);
+      }}
+      onError={(e) => console.log(JSON.parse(e.data))}
+      onClose={console.log}
+      />
+      <Text style={{fontSize: 18}}> {this.state.viewers + " Qube owners watching!"} </Text>
       </View>
     )
   }
